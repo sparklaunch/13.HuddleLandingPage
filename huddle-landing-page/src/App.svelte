4d50b658749b938a41963b69bd6d32e8d2e289f2
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-22 13:31:14 +0900

    Add the background

--- huddle-landing-page/src/App.svelte
+++ huddle-landing-page/src/App.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+    let innerWidth: number;
+    $: isMobile = innerWidth <= 1440;
+    $: document.body.id = isMobile ? "mobile-body" : "desktop-body";
+</script>
+
+<svelte:window bind:innerWidth />
 <div id="app" />
