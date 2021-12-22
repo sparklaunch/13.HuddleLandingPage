8882e70202f074e1c6660e6c02bdecfdc271fcfc
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-22 13:44:25 +0900

    Add the content

--- huddle-landing-page/src/components/Content.svelte
+++ huddle-landing-page/src/components/Content.svelte
@@ -1,1 +1,53 @@
-<div id="content" />
+<div id="content">
+    <div id="illustration">
+        <img src="/assets/illustration-mockups.svg" alt="Mockup Illustration" />
+    </div>
+    <div id="detail">
+        <h1>Build The Community Your Fans Will Love</h1>
+        <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+        </p>
+        <button>Register</button>
+    </div>
+</div>
+
+<style>
+    #content {
+        display: flex;
+    }
+    #illustration {
+        width: 50%;
+    }
+    #illustration > img {
+        width: 100%;
+    }
+    #detail {
+        width: 50%;
+        padding: 50px;
+    }
+    #detail > h1 {
+        color: white;
+        font-family: "Poppins", sans-serif;
+        font-size: 40px;
+        margin-bottom: 25px;
+    }
+    #detail > p {
+        color: white;
+        font-family: "Open Sans", sans-serif;
+        font-size: 18px;
+        line-height: 1.6;
+        margin-bottom: 20px;
+    }
+    #detail > button {
+        color: rgb(82, 58, 158);
+        background-color: white;
+        padding: 15px 60px;
+        font-family: "Poppins", sans-serif;
+        font-size: 20px;
+        border-radius: 100px;
+        cursor: pointer;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
+    }
+</style>
