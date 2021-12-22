66f556917f7f6adaee8b57eec7607fa0d0b9a672
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-22 13:35:29 +0900

    Add the logo

--- huddle-landing-page/src/App.svelte
+++ huddle-landing-page/src/App.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
+    import Logo from "./components/Logo.svelte";
+    import Content from "./components/Content.svelte";
+    import Footer from "./components/Footer.svelte";
     let innerWidth: number;
     $: isMobile = innerWidth <= 1440;
     $: document.body.id = isMobile ? "mobile-body" : "desktop-body";
 </script>
 
 <svelte:window bind:innerWidth />
-<div id="app" />
+<div id="app">
+    <div id="container">
+        <Logo />
+        <Content />
+        <Footer />
+    </div>
+</div>
+
+<style>
+    #app {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #container {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ huddle-landing-page/src/components/Logo.svelte
@@ -0,0 +1,9 @@
+<div id="logo">
+    <img src="/assets/logo.svg" alt="Logo" />
+</div>
+
+<style>
+    #logo {
+        align-self: flex-start;
+    }
+</style>
